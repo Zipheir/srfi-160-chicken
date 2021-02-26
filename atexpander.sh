166af6b8e6d4afd54f0dc89c6468fa70c2d4b48c
dieggsy <dieggsy@pm.me>
2021-02-25 23:18:05 -0500

    Use the atexpander for the wiki as well

--- atexpander.sh
+++ atexpander.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 # Expand library, implementation and test files
+echo -n "" > srfi-160.svnwiki
+
+< wiki/wiki-head.svnwiki >> srfi-160.svnwiki
+
 for at bytes in u8 1 s8 1 u16 2 s16 2 u32 4 s32 4 u64 8 s64 8 f32 4 f64 8 c64 8 c128 16; do
     from="from"
     to="to"
@@ -10,5 +14,8 @@ for at bytes in u8 1 s8 1 u16 2 s16 2 u32 4 s32 4 u64 8 s64 8 f32 4 f64 8 c64 8 
     sed "s/@/$at/g; s/&bytes/$bytes/g; s/&from/$from/g; s/&to/$to/g" at-impl.scm > at/$at-impl.scm
     sed "s/@/$at/g;" at-vector2list.scm > at/$at-vector2list.scm
     sed "s/@/$at/g;" srfi.160.at.scm > at/srfi.160.$at.scm
+
+    sed "s/@/$at/g;" wiki/at-module.svnwiki >> srfi-160.svnwiki
 done
 
+< wiki/wiki-tail.svnwiki >> srfi-160.svnwiki
