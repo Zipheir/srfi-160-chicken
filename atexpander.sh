84ac2645c1fb58613e26f83408e3aca07a943ace
dieggsy <dieggsy@pm.me>
2021-02-27 10:55:49 -0500

    Use the atexpander for tests
    - Use numbers that work for all vectors. This limits tests somewhat, but
      is useful and helped me catch some bugs.

--- atexpander.sh
+++ atexpander.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 # Expand library, implementation and test files
-echo -n "" > srfi-160.svnwiki
 
-< wiki/wiki-head.svnwiki >> srfi-160.svnwiki
+< wiki/wiki-head.svnwiki > srfi-160.svnwiki
+
+< tests/base-tests.scm > tests/run.scm
 
 for at bytes in u8 1 s8 1 u16 2 s16 2 u32 4 s32 4 u64 8 s64 8 f32 4 f64 8 c64 8 c128 16; do
     from="from"
@@ -16,6 +17,9 @@ for at bytes in u8 1 s8 1 u16 2 s16 2 u32 4 s32 4 u64 8 s64 8 f32 4 f64 8 c64 8 
     sed "s/@/$at/g;" srfi.160.at.scm > at/srfi.160.$at.scm
 
     sed "s/@/$at/g;" wiki/at-module.svnwiki >> srfi-160.svnwiki
+    sed "s/@/$at/g;" tests/at-tests.scm >> tests/run.scm
 done
 
 < wiki/wiki-tail.svnwiki >> srfi-160.svnwiki
+echo "(test-end)" >> tests/run.scm
+echo "(test-exit)" >> tests/run.scm
--- tests/chicken-base-tests.scm
+++
@@ -1,4 +0,0 @@
-(import (scheme))
-(import (srfi 160 base))
-
-(include-relative "shared-base-tests.scm")
